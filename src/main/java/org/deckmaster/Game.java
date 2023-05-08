a98a2f873eba6c6451a5663b341f07c6c2c00b44
190003505 <random@st-andrews.ac.uk>
2023-05-08 16:39:33 +0100

    updated multiple success condition events & proactive events.

--- src/main/java/org/deckmaster/Game.java
+++ src/main/java/org/deckmaster/Game.java
@@ -43,7 +43,7 @@ public class Game extends PApplet {
     @Override
     public void settings() {
         size(displayWidth, displayHeight, P2D);
-        fullScreen();
+//        fullScreen();
     }
 
     @Override
@@ -387,5 +387,8 @@ public class Game extends PApplet {
             }
 
         }
+
+        System.out.println("tracked events: " + trackedEvents.toString());
+        System.out.println("non-unique cards: " + nonUniqueCards.toString());
     }
 }
